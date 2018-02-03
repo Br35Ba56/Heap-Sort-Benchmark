717dbd44817162a52125a8ada1452f52158957ae
Anthony Schneider <tonyschndr@gmail.com>
2018-02-03 12:02:03 -0600

    added SortInterface.java to version control

---
+++ src/benchmark/SortInterface.java
@@ -0,0 +1,11 @@
+package benchmark;
+
+public interface SortInterface {
+
+    void recursiveSort(int[] list) throws UnsortedException;
+    void iterativeSort(int[] list) throws UnsortedException;
+
+    int getCount();
+    long getTime();
+
+}
